5be9fd5f52297ea2d25e55ea6628209852d50c59
allwaysapp <info@allways.app>
2025-07-17 02:39:54 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,1 +1,30 @@
-console.log("Custom kod çalışıyor.");
+(function() {
+    const menuLinks = document.querySelectorAll('a.menu--block-item');
+    
+    menuLinks.forEach(link => {
+        const span = link.querySelector('span.tb--menu-item_text');
+        if (span) {
+            const text = span.textContent.trim();
+            let icon = '';
+            
+            switch(text) {
+                case 'BONUS TALEP':
+                    icon = '<i class="fas fa-gift"></i> ';
+                    break;
+                case 'EVA TV':
+                    icon = '<i class="fas fa-tv"></i> ';
+                    break;
+                case 'EVA ÇARK':
+                    icon = '<i class="fas fa-dharmachakra"></i> ';
+                    break;
+                case 'WHATSAPP':
+                    icon = '<i class="fab fa-whatsapp"></i> ';
+                    break;
+            }
+            
+            if (icon) {
+                span.innerHTML = icon + text;
+            }
+        }
+    });
+})();
