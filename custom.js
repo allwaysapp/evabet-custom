684e131ba07e708f0cd7801b2cae0220dee7225a
allwaysapp <info@allways.app>
2025-07-17 05:04:30 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,11 +1,11 @@
-// Font Awesome yükle
-const fontAwesome = document.createElement('link');
-fontAwesome.rel = 'stylesheet';
-fontAwesome.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
-document.head.appendChild(fontAwesome);
+// Font Awesome yükle - farklı ad
+const faLink = document.createElement('link');
+faLink.rel = 'stylesheet';
+faLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
+document.head.appendChild(faLink);
 
-// Icon ekleme fonksiyonu  
-function addIcons() {
+// Icon ekleme fonksiyonu
+function addMenuIcons() {
     document.querySelectorAll('a.menu--block-item span.tb--menu-item_text').forEach(span => {
         if (!span.querySelector('i')) {
             const text = span.textContent.trim();
@@ -17,5 +17,14 @@ function addIcons() {
     });
 }
 
-// Sadece hemen çalıştır
-addIcons();
+// Hemen dene
+addMenuIcons();
+
+// Observer
+const menuObserver = new MutationObserver((mutations, obs) => {
+    if (document.querySelector('a.menu--block-item')) {
+        addMenuIcons();
+        obs.disconnect();
+    }
+});
+menuObserver.observe(document.body, { childList: true, subtree: true });
